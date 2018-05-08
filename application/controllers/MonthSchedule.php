ccb66862461463500554cd22cc60520e56ea3eba
Tushar <tushar@linkwok.com>
2018-05-08 10:52:57 +0530

    Month Schedule

--- application/controllers/MonthSchedule.php
+++ application/controllers/MonthSchedule.php
@@ -10,6 +10,7 @@ class MonthSchedule extends CI_Controller {
 
         //  Calling cURL Library
         $this->load->library('curl');
+        $this->load->model('Month_Schedule');
     }
 
     public function index() {
@@ -22,6 +23,27 @@ class MonthSchedule extends CI_Controller {
         if (!$this->session->userdata('employee_id')) {
             $data['error_message'] = "Please login first.";
             $this->load->view('login_user', $data);
+        }
+    }
+
+    public function month_schedule() {
+        $get_inventories = $this->Month_Schedule->get_inventories();
+        $get_batch_plan = $this->Month_Schedule->get_batch_plans();
+        //echo '<pre>';print_r($get_batch_plan);die();
+
+        if (isset($get_inventories)) {
+            if (!empty($get_inventories->success)) {
+                //echo '<pre>';print_r($get_inventories->inventories);die();
+                $view_data['batch_plans'] = $get_batch_plan['batch_plans'];
+                $view_data['inventories'] = $get_inventories->inventories;
+                $this->load->view('month_schedule_view', $view_data);
+            } else {
+                $view_data['error_message'] = $get_inventories->message;
+                $this->load->view('month_schedule_view', $view_data);
+            }
+        } else {
+            $view_data['error_message'] = 'No Responce';
+            $this->load->view('month_schedule_view', $view_data);
         }
     }
 
