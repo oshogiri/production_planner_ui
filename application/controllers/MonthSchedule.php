48bc163ae7ac42f8e3ba5036f9896251bfb323f4
Tushar <tushar@linkwok.com>
2018-05-09 12:29:12 +0530

    Month Schedule

--- application/controllers/MonthSchedule.php
+++ application/controllers/MonthSchedule.php
@@ -27,15 +27,30 @@ class MonthSchedule extends CI_Controller {
     }
 
     public function month_schedule() {
-        $get_inventories = $this->Month_Schedule->get_inventories();
-        $get_batch_plan = $this->Month_Schedule->get_batch_plans();
-        //echo '<pre>';print_r($get_batch_plan);die();
+        parse_str($_SERVER['QUERY_STRING'], $_GET);
+        if (isset($_GET['month']))
+            $month = $_GET['month'];
+        else
+            $month = null;
+
+        $get_inventories = $this->Month_Schedule->get_inventories($month);
+        $get_batch_plan = $this->Month_Schedule->get_batch_plans($month);
+        //echo '<pre>';print_r($month);die();
+
+        if (isset($month))
+            $condate = date('F Y', $month);
+        else
+            $condate = date('F Y');
+        $view_data['month'] = $condate;
+//        echo '<pre>';print_r($view_data['month']);die();
 
         if (isset($get_inventories)) {
             if (!empty($get_inventories->success)) {
                 //echo '<pre>';print_r($get_inventories->inventories);die();
                 $view_data['batch_plans'] = $get_batch_plan['batch_plans'];
                 $view_data['inventories'] = $get_inventories->inventories;
+
+                $view_data['date_header_array'] = $get_batch_plan['date_range'];
                 $this->load->view('month_schedule_view', $view_data);
             } else {
                 $view_data['error_message'] = $get_inventories->message;
