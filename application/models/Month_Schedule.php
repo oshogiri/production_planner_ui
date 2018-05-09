b317327ee28152b46ca1617925f6e8d9ff85defa
Tushar <tushar@linkwok.com>
2018-05-09 12:33:45 +0530

    Month schedule model

--- application/models/Month_Schedule.php
+++ application/models/Month_Schedule.php
@@ -4,11 +4,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Month_schedule extends CI_Model {
 
-    public function get_batch_plans() {
+    public function get_batch_plans($month = null) {
         date_default_timezone_set('Asia/Kolkata');
+        
+        $url = 'http://172.16.0.22:1313/api/v1/batch_plans/get_batch_plans';
+        if(isset($month))
+            $url = $url.'?batch_plan_date='.$month;
 
         //  Setting URL To Fetch Data From
-        $this->curl->create('http://172.16.0.22:1313/api/v1/batch_plans/get_batch_plans');
+        $this->curl->create($url);
 
         //  To Temporarily Store Data Received From Server
         $this->curl->option('buffersize', 10);
@@ -30,13 +34,17 @@ class Month_schedule extends CI_Model {
 
         $decode_data = json_decode($data, true);
         //echo '<pre>';print_r($decode_data);die();
-
+        
         return $decode_data;
     }
     
-    public function get_inventories() {
+    public function get_inventories($month = null) {
+        $url = 'http://172.16.0.22:1313/api/v1/inventories/get_inventories';
+        if(isset($month))
+            $url = $url.'?inventory_date='.$month;
+        
         //  Setting URL To Fetch Data From
-        $this->curl->create('http://172.16.0.22:1313/api/v1/inventories/get_inventories');
+        $this->curl->create($url);
 
         //  To Temporarily Store Data Received From Server
         $this->curl->option('buffersize', 10);
@@ -57,7 +65,7 @@ class Month_schedule extends CI_Model {
         $data = $this->curl->execute();
 
         $decode_data = json_decode($data);
-        //echo '<pre>';print_r($decode_data);die();
+//        echo '<pre>';print_r($url);die();
 
         return $decode_data;
     }
