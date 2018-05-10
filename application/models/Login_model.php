cfeb679c6aa8b46e1041a6d41b85a1b104d0afb5
Tushar <tushar@linkwok.com>
2018-05-10 17:08:32 +0530

    Forget password

--- application/models/Login_model.php
+++ application/models/Login_model.php
@@ -71,6 +71,6 @@ class Login_model extends CI_Model {
         $this->email->subject("Reset your Password");
         $message = "<p>This email has been sent as a request to reset our password</p>";
         $message .= "<p><a href='" . base_url() . "forgetpassword/$email'>Click here </a>if you want to reset your password, if not, then ignore</p>";
-        $this->send();
+        $this->email->send();
     }
 }
