7af12cdb4f80af863d9d4b0333b67c7e008c4e71
tusharsinh <tusharsinh@tusharsinh-Lenovo-G50-80>
2018-10-08 12:21:18 +0530

    database credentials

--- application/config/database.php
+++ application/config/database.php
@@ -75,7 +75,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
+	'hostname' => '172.16.0.22',
 	'username' => 'root',
 	'password' => '1linkwok',
 	'database' => 'ps',
