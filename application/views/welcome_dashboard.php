28271807d24dd797239533a950244bb174d72bef
tusharsinh <tusharsinh@tusharsinh-Lenovo-G50-80>
2018-11-01 16:28:12 +0530

    PP Manual

--- application/views/welcome_dashboard.php
+++ application/views/welcome_dashboard.php
@@ -6,6 +6,17 @@
     <div class="page-title-wrapper">
         <h2 class="page-title">Dashboard</h2>
     </div><!-- /.page-titile-wrapper -->
+    
+    <div class="panel-body">
+        <h4>Download User Manual</h4>
+        <div class="row">
+            <div class="col-sm-2">
+                <a href="<?php echo base_url() ?>sample/ProductionPlannermanual.pdf" class="thumbnail" target="_blank">
+                    <img src="<?php echo base_url() ?>sample/pdf.png" alt="150x150" data-holder-rendered="true" style="width: 150px; height: 150px;">
+                </a>
+            </div>
+        </div>
+    </div>
 </div>
 
 <?php require_once 'footer.php'; ?>
